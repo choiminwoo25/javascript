ed55bac5a01208e8979c4f8a7275011e4d4c10b3
choiminwoo25 <cmotwin1108@naver.com>
2026-06-05 20:05:26 +0900

    커밋

---
+++ practice/src/components/Button.tsx
@@ -0,0 +1,10 @@
+export default function Button(
+    {text , onClick} : {text : string ; 
+        onClick : () => void}
+) {
+    return(
+        <>
+        <button onClick={onClick}>{text}</button>
+        </>
+    )
+}
---
+++ practice/src/components/Title.tsx
@@ -0,0 +1,7 @@
+export default function Title() {
+    return(
+        <>
+        <h1>학생정보</h1>
+        </>
+    )
+}
